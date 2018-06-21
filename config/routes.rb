bf5c41a0272130091ea50983f19c1c11e1f8b808
AyyLam <al3101@nyu.edu>
2018-06-21 15:39:40 -0400

    Added edit and delete for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :users
-  resources :trucks
-  resources :reviews, only: [:show, :new, :create]
-  resources :neighborhoods
+  resources :users, only: [:show, :new, :create]
+  resources :trucks, only: [:index, :show, :new, :create]
+  resources :reviews, only: [:new, :create, :edit, :update, :destroy]
+  resources :neighborhoods, only: [:index, :show, :new, :create]
 
 
   get 'login', to: 'sessions#new', as: 'login'
